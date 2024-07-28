dc1d5b7a84fc13f589f787bcd1dd16a654a72d12
Siva Naru <sivanaru99@gmail.com>
2024-07-28 16:47:28 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,30 @@
+package p1;
+public class ATM {
+    private BankAccount account;
+
+    public ATM(BankAccount account) {
+        this.account = account;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && account.getBalance() >= amount) {
+            account.withdraw(amount);
+            System.out.println("Withdrawal of " + amount + " was successful.");
+        } else {
+            System.out.println("Insufficient balance or invalid amount.");
+        }
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            account.deposit(amount);
+            System.out.println("Deposit of " + amount + " was successful.");
+        } else {
+            System.out.println("Invalid amount.");
+        }
+    }
+
+    public void checkBalance() {
+        System.out.println("Your current balance is: " + account.getBalance());
+    }
+}
---
+++ BankAccount.java
@@ -0,0 +1,28 @@
+package p1;
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        if (initialBalance > 0) {
+            this.balance = initialBalance;
+        } else {
+            this.balance = 0;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+        }
+    }
+}
